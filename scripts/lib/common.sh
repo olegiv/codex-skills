206f763afaa6324efbf689f79ce3e1cd847cb006
Oleg Ivanchenko <olegiv@users.noreply.github.com>
2026-02-23 20:02:40 +0100

    Quote trim expansions for shellcheck
    Quote parameter expansions in trim() to satisfy SC2295 and keep string trimming behavior unchanged in CI shellcheck runs.

--- scripts/lib/common.sh
+++ scripts/lib/common.sh
@@ -17,8 +17,8 @@ die() {
 
 trim() {
   local value="$1"
-  value="${value#${value%%[![:space:]]*}}"
-  value="${value%${value##*[![:space:]]}}"
+  value="${value#"${value%%[![:space:]]*}"}"
+  value="${value%"${value##*[![:space:]]}"}"
   printf '%s' "$value"
 }
 
